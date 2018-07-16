438d2eb133f23d314b648170441c65a9c14ac6cc
javier <cardozojavier.c@gmail.com>
2018-07-16 19:36:50 -0300

    Update

---
+++ Reverse Letters in Sentence/script.js
@@ -0,0 +1,17 @@
+function reverser(sentence) {
+  var array=[];
+  var resul=[];
+  for (var i=0; i<sentence.length; i++){
+    if (sentence[i] != ' '){
+      array.unshift(sentence[i]);
+      if (i == sentence.length-1){
+        resul.push(array.join(''));
+      }
+    }else{
+      array.push(' ');
+      resul.push(array.join(''));
+      array=[];
+    }
+  }
+  return resul.join(''); 
+}

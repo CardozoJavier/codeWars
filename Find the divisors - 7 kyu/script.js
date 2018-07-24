6bc408de3bfa560cbb356b29def9606816c797ab
javier <cardozojavier.c@gmail.com>
2018-07-24 20:07:34 -0300

    New kata

---
+++ Find the divisors - 7 kyu/script.js
@@ -0,0 +1,12 @@
+function divisors(integer){
+  var arr=[];
+  for (var i= integer-1; i >1; i--){
+    if ((integer/i) % 1 == 0){
+        arr.push(integer/i);
+    }
+  }
+  if (arr.length == 0){
+    return integer + ' is prime';
+  }
+  return arr;
+}
